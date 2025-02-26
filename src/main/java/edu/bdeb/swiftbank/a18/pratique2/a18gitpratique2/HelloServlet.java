9e61f8eeab79e8894e68eb0505e4e5b12701a31e
Evgenii Morgunov <MorgunovE87@gmail.com>
2025-02-26 10:58:01 -0500

    add files

---
+++ src/main/java/edu/bdeb/swiftbank/a18/pratique2/a18gitpratique2/HelloServlet.java
@@ -0,0 +1,28 @@
+package edu.bdeb.swiftbank.a18.pratique2.a18gitpratique2;
+
+import java.io.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", urlPatterns = {"/hello-servlet"})
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    @Override
+    public void init() {
+        message = "Hello World!";
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
